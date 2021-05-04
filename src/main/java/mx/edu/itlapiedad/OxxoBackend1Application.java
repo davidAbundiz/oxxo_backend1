47c01d40b17ca126b52f5c6e2c2ed61b69b2995f
David Abundiz <davidAbundiz_lq@hotmail.com>
2021-05-04 00:24:16 -0500

    Commit inicial oxxo_backend1

---
+++ src/main/java/mx/edu/itlapiedad/OxxoBackend1Application.java
@@ -0,0 +1,13 @@
+package mx.edu.itlapiedad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxxoBackend1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxxoBackend1Application.class, args);
+	}
+
+}
